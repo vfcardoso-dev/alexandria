77b236fc96b8e83edd1f660f5f6b45538637295f
Gabriel <sana.gp@gmail.com>
2023-10-23 22:20:09 -0300

    Creating some controls at loan screen. WIP.

--- ui/src/app/private/components/loan-create/loan-create.component.ts
+++ ui/src/app/private/components/loan-create/loan-create.component.ts
@@ -2,8 +2,11 @@ import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 import { Component, ElementRef, OnInit, ViewChild } from "@angular/core";
 import { FormControl, FormGroup, Validators } from "@angular/forms";
 import { Router } from "@angular/router";
-import { catchError, Observable, throwError } from "rxjs";
+import { catchError, filter, Observable, throwError } from "rxjs";
 import { environment } from "src/environments/environment";
+import { MemberGridListModel } from "../member-list/member-list.component";
+import { TitleGridModel } from "../title-list/title-list.component";
+import { CopyListModel } from "../copy-list/copy-list.component";
 
 
 export interface LoanForm {
@@ -20,9 +23,18 @@ export interface LoanForm {
 export class LoanCreateComponent implements OnInit{
 
     @ViewChild('input') input: ElementRef<HTMLInputElement>;
-    myControl = new FormControl('');
-    options: string[] = ['One', 'Two', 'Three', 'Four', 'Five'];
-    filteredOptions: string[];
+    myControl = new FormControl(''); 
+    myControl2 = new FormControl('');      
+    
+    memberData: MemberGridListModel[] = [];
+    filteredMemberData: MemberGridListModel[];
+
+    copyData: CopyListModel[] = [];
+    filteredCopyData: CopyListModel[];
+
+    memberId: string;
+    copyId: string;
+
 
 
     public form: FormGroup = new FormGroup<LoanForm>({ 
@@ -31,17 +43,52 @@ export class LoanCreateComponent implements OnInit{
 
     
     constructor(private http: HttpClient, private router:Router){ 
-        this.filteredOptions = this.options.slice();     
+        this.filteredMemberData = this.memberData.slice();     
     }
 
     ngOnInit() {      
-                       
+        this.loadMemberData();
+        this.loadCopyData();                   
     }
 
-    public filter(): void {
-        const filterValue = this.input.nativeElement.value.toLowerCase();
-        this.filteredOptions = this.options.filter(o => o.toLowerCase().includes(filterValue));
-      }
+    public loadMemberData = () => {
+        this.http.post<MemberGridListModel[]>(`${environment.apiUrl}/api/member/grid/list.json`, {  }).subscribe(data => {                                              
+          this.memberData = data;
+        });        
+    }
+
+    public filterMember(input:any): void {
+        const filterValue = input.value.toLowerCase();
+        this.filteredMemberData = this.memberData.filter(m => m.name.toLowerCase().includes(filterValue));
+    }
+
+    public setSelectedMember(member: MemberGridListModel){
+        this.memberId = member.id;
+    }
+        
+    public loadCopyData = () => {
+        this.http.post<CopyListModel[]>(`${environment.apiUrl}/api/copy/grid/list.json`, {  }).subscribe(data => {                                              
+            this.copyData = data;                        
+        });        
+    }
+    
+    public filterCopy(input:any): void {
+        const filterValue = input.value.toLowerCase();        
+        this.filteredCopyData = 
+            this.copyData.filter(m => 
+                m.number.toString().includes(filterValue) ||            
+                m.titleModel.name.toLowerCase().includes(filterValue));         
+    }
+
+    public setSelectedCopy(copy: CopyListModel){
+        this.copyId = copy.id;
+    }
+
+    public displayCopyName(copy: CopyListModel): string {
+        let name = '('+copy.number+') ';
+        name+= copy.titleModel.name+' ';         
+        return name;
+    }
 
     private handleError(error: HttpErrorResponse) {
         if (error.status === 0) {
