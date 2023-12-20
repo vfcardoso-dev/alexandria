0909484b47aa6828d857583cb3e63312655ef26c
Gabriel <sana.gp@gmail.com>
2023-12-19 23:24:33 -0300

    Removing auto ddl hbernate feature and creting migration file.

---
+++ database/migrations/2023-00-00.sql
@@ -0,0 +1,108 @@
+
+
+IF OBJECT_ID('dbo.user', 'U') IS NULL
+BEGIN
+	CREATE TABLE  [dbo].[user] 
+	(
+		id UNIQUEIDENTIFIER NOT NULL PRIMARY KEY DEFAULT NEWID(),
+		email VARCHAR(255) NULL,
+		[name] VARCHAR(255) NOT NULL,
+		[password] VARCHAR(255) NOT NULL,
+		createdAt DATETIME2(7) NULL,
+		[version] DATETIME2(7) NULL
+	)
+END;
+
+IF OBJECT_ID('dbo.address', 'U') IS NULL
+BEGIN
+	CREATE TABLE  [dbo].[address]
+	(
+		id UNIQUEIDENTIFIER NOT NULL PRIMARY KEY DEFAULT NEWID(),
+		[address] VARCHAR(255) NULL,
+		city VARCHAR(255) NOT NULL,
+		details VARCHAR(255) NOT NULL,
+		[number] VARCHAR(255) NULL,
+		postal_code VARCHAR(255) NULL,
+		state_province VARCHAR(255) NULL,
+		[version] DATETIME2(7) NULL
+	)
+END;
+
+IF OBJECT_ID('dbo.member', 'U') IS NULL
+BEGIN
+	CREATE TABLE  [dbo].[member]
+	(
+		id UNIQUEIDENTIFIER NOT NULL PRIMARY KEY DEFAULT NEWID(),
+		[name] VARCHAR(255) NULL,
+		birth_date DATETIME2(7) NOT NULL,
+		email VARCHAR(255) NULL,
+		occupation VARCHAR(255) NULL,
+		phone VARCHAR(255) NULL,
+		registration_number VARCHAR(255) NULL,
+		address_id UNIQUEIDENTIFIER NULL,
+		[version] DATETIME2(7) NULL
+	)
+END;
+--TODO: Create FKs
+
+
+IF OBJECT_ID('dbo.author', 'U') IS NULL
+BEGIN
+	CREATE TABLE  [dbo].[author]
+	(
+		id UNIQUEIDENTIFIER NOT NULL PRIMARY KEY DEFAULT NEWID(),
+		[name] VARCHAR(255) NULL,
+		pseudonym VARCHAR(255) NULL,
+		last_name VARCHAR(255) NULL
+	)
+END;
+
+IF OBJECT_ID('dbo.title', 'U') IS NULL
+BEGIN
+	CREATE TABLE  [dbo].[title]
+	(
+		id UNIQUEIDENTIFIER NOT NULL PRIMARY KEY DEFAULT NEWID(),
+		[name] VARCHAR(255) NULL
+	)
+END;
+
+IF OBJECT_ID('dbo.author_title', 'U') IS NULL
+BEGIN
+	CREATE TABLE  [dbo].[author_title]
+	(
+		author_id UNIQUEIDENTIFIER NOT NULL,
+		title_id UNIQUEIDENTIFIER NOT NULL
+
+		CONSTRAINT PK_author_title PRIMARY KEY (author_id, title_id)
+	)
+END;
+
+IF OBJECT_ID('dbo.loan', 'U') IS NULL
+BEGIN
+	CREATE TABLE  [dbo].[loan]
+	(
+		id UNIQUEIDENTIFIER NOT NULL PRIMARY KEY DEFAULT NEWID(),
+		[date] DATETIME2(7) NOT NULL DEFAULT GETDATE(),
+		expiring_date DATETIME2(7) NOT NULL,
+		return_date DATETIME2(7) NULL,
+		copy_id UNIQUEIDENTIFIER NOT NULL,
+		member_id UNIQUEIDENTIFIER NOT NULL
+	)
+END;
+--TODO: Create FKs
+
+IF OBJECT_ID('dbo.copy', 'U') IS NULL
+BEGIN
+	CREATE TABLE  [dbo].[copy]
+	(
+		id UNIQUEIDENTIFIER NOT NULL PRIMARY KEY DEFAULT NEWID(),
+		[number] int NULL,
+		title_id UNIQUEIDENTIFIER NOT NULL
+	)
+END;
+--TODO: Create FKs
+
+
+
+
+
